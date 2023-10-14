b9e25837086641c257a2faa2066576d161e3f05e
Hencuy <henrylopezllave27@gmail.com>
2023-10-14 03:22:04 -0300

    Ventana Main Finalizada

--- Dashboard_WPF/MainWindow.xaml.cs
+++ Dashboard_WPF/MainWindow.xaml.cs
@@ -13,7 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Dashboard_WPF.Views;
+using Dashboard_WPF.Views.Categorias;
+using Dashboard_WPF.Views.Clientes;
+using Dashboard_WPF.Views.Compras;
 using Dashboard_WPF.Views.Dashboard;
+using Dashboard_WPF.Views.Kardex;
+using Dashboard_WPF.Views.Productos;
+using Dashboard_WPF.Views.Proveedores;
+using Dashboard_WPF.Views.Reportes;
+using Dashboard_WPF.Views.Usuarios;
+using Dashboard_WPF.Views.Ventas;
 
 namespace Dashboard_WPF
 {
@@ -22,22 +31,26 @@ namespace Dashboard_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        PDashboard pDashboard = new PDashboard();
+        VPCategoria vpCategorias = new VPCategoria();
+        VPClientes vpClientes = new VPClientes();
+        VPCompras vpCompras = new VPCompras();
+        VPKardex vpKardex = new VPKardex();
+        VPProductos vpProductos = new VPProductos();
+        VPProveedores vpProveedores = new VPProveedores();
+        VPReportes vpReportes = new VPReportes();
+        VPUsuarios vpUsuarios = new VPUsuarios();
+        VPVentas vpVentas = new VPVentas();
         
         public MainWindow()
         {
             InitializeComponent();
+            MyFrame.NavigationService.Navigate(new PDashboard(MyFrame));
         }
 
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
             base.OnMouseLeftButtonDown(e);
             DragMove();
-        }
-
-        private void btnDash_Click(object sender, RoutedEventArgs e)
-        {
-            MyFrame.NavigationService.Navigate(pDashboard);
         }
 
         private void btnMinimizar_Click(object sender, RoutedEventArgs e)
@@ -48,6 +61,55 @@ namespace Dashboard_WPF
         private void btnCerrar_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+        private void btnDash_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.NavigationService.Navigate(new PDashboard(MyFrame));
+        }
+
+        private void btnCategorias_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.NavigationService.Navigate(vpCategorias);
+        }
+
+        private void btnProvee_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.NavigationService.Navigate(vpProveedores);
+        }
+
+        private void btnCompras_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.NavigationService.Navigate(vpCompras);
+        }
+
+        private void btnUsuarios_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.NavigationService.Navigate(vpUsuarios);
+        }
+
+        private void btnClientes_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.NavigationService.Navigate(vpClientes);
+        }
+
+        private void btnProductos_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.NavigationService.Navigate(vpProductos);
+        }
+
+        private void btnVentas_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.NavigationService.Navigate(vpVentas);
+        }
+
+        private void btnKardex_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.NavigationService.Navigate(vpKardex);
+        }
+
+        private void btnReportes_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.NavigationService.Navigate(vpReportes);
         }
     }
 }
--- Dashboard_WPF/Views/Dashboard/PDashboard.xaml.cs
+++ Dashboard_WPF/Views/Dashboard/PDashboard.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿using Dashboard_WPF.Views.Categorias;
+using Dashboard_WPF.Views.Clientes;
+using Dashboard_WPF.Views.Compras;
+using Dashboard_WPF.Views.Kardex;
+using Dashboard_WPF.Views.Productos;
+using Dashboard_WPF.Views.Proveedores;
+using Dashboard_WPF.Views.Reportes;
+using Dashboard_WPF.Views.Usuarios;
+using Dashboard_WPF.Views.Ventas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +29,66 @@ namespace Dashboard_WPF.Views.Dashboard
     /// </summary>
     public partial class PDashboard : Page
     {
-        public PDashboard()
+        public Frame mainFrame;
+
+        VPCategoria vpCategorias = new VPCategoria();
+        VPClientes vpClientes = new VPClientes();
+        VPCompras vpCompras = new VPCompras();
+        VPKardex vpKardex = new VPKardex();
+        VPProductos vpProductos = new VPProductos();
+        VPProveedores vpProveedores = new VPProveedores();
+        VPReportes vpReportes = new VPReportes();
+        VPUsuarios vpUsuarios = new VPUsuarios();
+        VPVentas vpVentas = new VPVentas();
+        public PDashboard(Frame myframe)
         {
             InitializeComponent();
+            mainFrame = myframe;
+        }
+
+        private void btnDashCategoria_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(vpCategorias);
+        }
+
+        private void btnDashProve_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(vpProveedores);
+        }
+
+        private void btnDashCompras_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(vpCompras);
+        }
+
+        private void btnDashUsuarios_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(vpUsuarios);
+        }
+
+        private void btnDashClientes_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(vpClientes);
+        }
+
+        private void btnDashProductos_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(vpProductos);
+        }
+
+        private void btnDashVentas_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(vpVentas);
+        }
+
+        private void btnDashKardex_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(vpKardex);
+        }
+
+        private void btnDashReportes_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(vpReportes);
         }
     }
 }
