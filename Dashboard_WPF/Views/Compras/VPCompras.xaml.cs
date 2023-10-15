309db087eadf98cb778e0ca0a30e68c6b422c881
Hencuy <henrylopezllave27@gmail.com>
2023-10-15 20:47:21 -0300

    "Se agrega Interfaz Compras"

--- Dashboard_WPF/Views/Compras/VPCompras.xaml.cs
+++ Dashboard_WPF/Views/Compras/VPCompras.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dashboard_WPF.Views.Proveedores;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,34 @@ namespace Dashboard_WPF.Views.Compras
     /// </summary>
     public partial class VPCompras : Page
     {
+        SubVCompras1 subVCompras1 = new SubVCompras1 ();
+        SubVCompras2 subVCompras2 = new SubVCompras2 ();
+        SubVCompras3 subVCompras3 = new SubVCompras3 ();
         public VPCompras()
         {
             InitializeComponent();
+            FrameCompras.NavigationService.Navigate(subVCompras1);
+        }
+
+        private void btnRegistrarCompra_Click(object sender, RoutedEventArgs e)
+        {
+            Titulo.Text = "NUEVA COMPRA";
+            Icono.Kind = MaterialDesignThemes.Wpf.PackIconKind.Shopping;
+            FrameCompras.NavigationService.Navigate(subVCompras1);
+        }
+
+        private void btnListaCompras_Click(object sender, RoutedEventArgs e)
+        {
+            Titulo.Text = "COMPRAS REALIZADAS";
+            Icono.Kind = MaterialDesignThemes.Wpf.PackIconKind.ListBox;
+            FrameCompras.NavigationService.Navigate(subVCompras2);
+        }
+
+        private void btnBuscarCompra_Click(object sender, RoutedEventArgs e)
+        {
+            Titulo.Text = "BUSCAR COMPRA";
+            Icono.Kind = MaterialDesignThemes.Wpf.PackIconKind.Magnify;
+            FrameCompras.NavigationService.Navigate(subVCompras3);
         }
     }
 }
