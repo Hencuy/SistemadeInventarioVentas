68ecca60ee1ca9d409cebfed041b65cfb00e95d8
EduSue <edu793.c.m@gmail.com>
2023-10-16 18:29:32 -0400

    Reportes Terminado

--- Dashboard_WPF/Views/Reportes/VPReportes.xaml.cs
+++ Dashboard_WPF/Views/Reportes/VPReportes.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dashboard_WPF.Views.Kardex;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,28 @@ namespace Dashboard_WPF.Views.Reportes
     /// </summary>
     public partial class VPReportes : Page
     {
+        SubVReportes1 subVReportes1 = new SubVReportes1();
+        SubVReportes2 subVReportes2 = new SubVReportes2();
+        SubVReportes3 subVReportes3 = new SubVReportes3();
         public VPReportes()
         {
             InitializeComponent();
+            FrameProveedores.NavigationService.Navigate(subVReportes1);
+        }
+
+        private void btnReporteGeneral_Click(object sender, RoutedEventArgs e)
+        {
+            FrameProveedores.NavigationService.Navigate(subVReportes1);
+        }
+
+        private void btnReporteInven_Click(object sender, RoutedEventArgs e)
+        {
+            FrameProveedores.NavigationService.Navigate(subVReportes2);
+        }
+
+        private void btnReporteCosto_Click(object sender, RoutedEventArgs e)
+        {
+            FrameProveedores.NavigationService.Navigate(subVReportes3);
         }
     }
 }
