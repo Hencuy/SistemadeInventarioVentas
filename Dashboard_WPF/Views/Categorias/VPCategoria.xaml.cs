995a122f3b318a3c59f2a85a436a6e83c51ad3cc
Bismar Nogales Vallejos <nogalesvallejosbismar@gmail.com>
2023-10-16 10:18:49 -0400

    vista categoria hecho

--- Dashboard_WPF/Views/Categorias/VPCategoria.xaml.cs
+++ Dashboard_WPF/Views/Categorias/VPCategoria.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dashboard_WPF.Views.Proveedores;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,28 @@ namespace Dashboard_WPF.Views.Categorias
     /// </summary>
     public partial class VPCategoria : Page
     {
+        subVCategoraias1 subVCategoraias1 = new subVCategoraias1();
+        subVCategoraias2 subVCategoraias2 = new subVCategoraias2();
+        subVCategoraias3 subVCategoraias3 = new subVCategoraias3();
         public VPCategoria()
         {
             InitializeComponent();
+            FrameCategorias.NavigationService.Navigate(subVCategoraias1);
+        }
+
+        private void C1_Click(object sender, RoutedEventArgs e)
+        {
+            FrameCategorias.NavigationService.Navigate(subVCategoraias1);
+        }
+
+        private void C2_Click(object sender, RoutedEventArgs e)
+        {
+            FrameCategorias.NavigationService.Navigate(subVCategoraias2);
+        }
+
+        private void C3_Click(object sender, RoutedEventArgs e)
+        {
+            FrameCategorias.NavigationService.Navigate(subVCategoraias3);
         }
     }
 }
