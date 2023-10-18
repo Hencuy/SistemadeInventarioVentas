819b12d2cc02f408d2c7a7cbaf6588690bacb1d7
Marco Antonio Sena Jacome <sjm3005591@est.univalle.edu>
2023-10-18 07:28:23 -0400

    .

--- Dashboard_WPF/Views/Productos/Sub_EnAlmacen.xaml.cs
+++ Dashboard_WPF/Views/Productos/Sub_EnAlmacen.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Dashboard_WPF.Modelos;
+using Dashboard_WPF.Views.Proveedores;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +19,121 @@ using System.Windows.Shapes;
 
 namespace Dashboard_WPF.Views.Productos
 {
-    /// <summary>
-    /// Lógica de interacción para Sub_EnAlmacen.xaml
-    /// </summary>
     public partial class Sub_EnAlmacen : Page
     {
+        SqlConnection conexion = new SqlConnection("Server= (LocalDB)\\MSSQLLocalDB;Database=BDInventarioVenta;Integrated Security=true; TrustServerCertificate=True");
         public Sub_EnAlmacen()
         {
             InitializeComponent();
+            CargarTabla();
+        }
+
+        private void CargarTabla()
+        {
+            string consulta = "SELECT NIT, NombreCompañia, NombreEncargado, Estado, TelefonoEncargado, Direccion, Email FROM Proveedor";
+
+            conexion.Open();
+            SqlCommand command = new SqlCommand(consulta, conexion);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            System.Data.DataTable dataTable = new System.Data.DataTable();
+
+
+            dataAdapter.Fill(dataTable);
+            dataTable.Columns.Add("Number", typeof(int));
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                dataTable.Rows[i]["Number"] = i + 1;
+
+            }
+            // Agregar columna de enumeración
+
+
+            membersDataGrid.ItemsSource = dataTable.DefaultView;
+
+            conexion.Close();
+        }
+
+        private void btnActualizar_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            string nit;
+            // Obtén el botón que se hizo clic
+            Button button = (Button)sender;
+
+            // Obtén la fila que contiene el botón
+            DataGridRow dataGridRow = FindParent<DataGridRow>(button);
+
+            // Verifica si la fila no es nula
+            if (dataGridRow != null)
+            {
+                // Obtén el objeto de datos de la fila seleccionada
+                DataRowView rowView = (DataRowView)dataGridRow.Item;
+
+                // Accede al valor del campo "CODIGO HOJA DE RUTA" (ID_Tramites)
+                nit = rowView["NIT"].ToString();
+
+                CambiarEstadoProveedor(nit);
+                CargarTabla();
+
+
+            }
+        }
+
+        private void CambiarEstadoProveedor(string nit)
+        {
+            try
+            {
+                // Abre la conexión a la base de datos
+                conexion.Open();
+
+                // Consulta SQL para actualizar el estado del proveedor
+                string consulta = "UPDATE Proveedor SET Estado = 0 WHERE NIT = @NIT";
+
+                using (SqlCommand comando = new SqlCommand(consulta, conexion))
+                {
+                    // Agrega el parámetro NIT a la consulta SQL
+                    comando.Parameters.AddWithValue("@NIT", nit);
+
+                    // Ejecuta la consulta
+                    int filasAfectadas = comando.ExecuteNonQuery();
+
+                    // Verifica si se actualizó al menos una fila
+                    if (filasAfectadas > 0)
+                    {
+                        MessageBox.Show("Proveedor Eliminado correctamente.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se pudo eliminar al proveedor.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cambiar el estado del proveedor: " + ex.Message);
+            }
+            finally
+            {
+                // Cierra la conexión a la base de datos
+                conexion.Close();
+            }
+        }
+
+        private T FindParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            DependencyObject parent = VisualTreeHelper.GetParent(child);
+
+            while (parent != null && !(parent is T))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return parent as T;
         }
     }
 }
